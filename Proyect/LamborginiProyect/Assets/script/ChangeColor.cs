83b668ca57cbf7e2e5ad70309cac70a244f8d8fa
WalterVale <walterjesusvale@gmail.com>
2016-04-13 08:16:35 -0300

    script

---
+++ Proyect/LamborginiProyect/Assets/script/ChangeColor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+
+    public Transform CarModel;
+    private enum Chasi { chasisv3, chasis1 ,chasis_0, baul, capo, chasis_externo, parachoque, puerta_1, puertas_2 }
+    void Start()
+    {
+        ChangeColorMaterial(Color.red);
+    }
+    public void ColorRed() {
+        ChangeColorMaterial(Color.red);
+    }
+    public void ColorYellow()
+    {
+        ChangeColorMaterial(Color.yellow);
+    }
+    public void ColorGray()
+    {
+        ChangeColorMaterial(Color.gray);
+    }
+    public void ColorBlue()
+    {
+       
+        ChangeColorMaterial(Color.green);
+    }
+    public void ChangeColorMaterial(Color color) {
+        MeshRenderer[] meshRenderer = CarModel.GetComponentsInChildren<MeshRenderer>(true);
+        foreach (MeshRenderer component in meshRenderer)
+        {
+            if (component.name.Equals(Chasi.chasisv3.ToString())) {
+                component.sharedMaterial.color = color;
+            }
+            if (component.name.Equals(Chasi.chasis1.ToString()))
+            {
+                component.sharedMaterial.color = color;
+            }
+            if (component.name.Equals(Chasi.chasis_0.ToString()))
+            {
+                component.sharedMaterial.color = color;
+            }
+            if (component.name.Equals(Chasi.baul.ToString()))
+            {
+                component.sharedMaterial.color = color;
+            }
+            if (component.name.Equals(Chasi.capo.ToString()))
+            {
+                component.sharedMaterial.color = color;
+            }
+            if (component.name.Equals(Chasi.chasis_externo.ToString()))
+            {
+                component.sharedMaterial.color = color;
+            }
+            if (component.name.Equals(Chasi.parachoque.ToString()))
+            {
+                component.sharedMaterial.color = color;
+            }
+            if (component.name.Equals(Chasi.puerta_1.ToString()))
+            {
+                component.sharedMaterial.color = color;
+            }
+            if (component.name.Equals(Chasi.puertas_2.ToString()))
+            {
+                component.sharedMaterial.color = color;
+            }
+
+        }
+    }
+}
---
+++ Proyect/LamborginiProyect/Assets/script/ControllerBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerBar : MonoBehaviour {
+
+    public Transform PanelBar;
+    public bool showPanelBar;
+    void Start()
+    {
+        PanelBar.gameObject.SetActive(false);
+    }
+    public void ShowOrHiddeBar()
+    {
+        PanelBar.gameObject.SetActive(showPanelBar);
+        showPanelBar = !showPanelBar;
+    }
+}
